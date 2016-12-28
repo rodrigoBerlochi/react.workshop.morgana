a820d09c8ca2a959538d3dfaf40e2367f506289a
Rodrigo Berlochi <v-rberlochi@tableau.com>
2016-12-28 12:11:16 -0300

    customizing  init page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React, Morgana!</h2>
+          <p>It is an exercise to learn by doing, using only Facebook tools: <a href="https://facebook.github.io/react/" target="_blank">React</a> + <a href="https://facebook.github.io/flux/docs/flux-utils.html#content" target="_blank">Flux Utils</a> + create-react-app</p>
+          <p>Potencially, you have <a href="https://flowtype.org/" target="_blank">Flow</a> and <a href="https://facebook.github.io/immutable-js/" target="_blank">Immutable.js</a>, also by Facebook.</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
